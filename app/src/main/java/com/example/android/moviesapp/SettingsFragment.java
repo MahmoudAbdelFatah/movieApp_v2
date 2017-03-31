ad074a72115458409dfff7df1b66818d234330d9
Mahmoud AbdelFatah <abdelfatahm3@gmail.com>
2017-03-31 20:37:01 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/android/moviesapp/SettingsFragment.java
@@ -0,0 +1,15 @@
+package com.example.android.moviesapp;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * Created by Mahmoud on 3/31/2017.
+ */
+
+public class SettingsFragment extends PreferenceFragmentCompat{
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+        addPreferencesFromResource(R.xml.pref_settings);
+    }
+}
