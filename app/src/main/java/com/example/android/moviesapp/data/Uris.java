a7a9ec62af62bbc0236e9ae92fc895181d3cd9da
Mahmoud-AbdelFatah <abdelfatahm3@gmail.com>
2017-03-31 11:16:11 +0200

    stage1

--- app/src/main/java/com/example/android/moviesapp/data/Uris.java
+++ app/src/main/java/com/example/android/moviesapp/data/Uris.java
@@ -7,7 +7,8 @@ package com.example.android.moviesapp.data;
 public class Uris {
     //Uris of popular or top rated
     public static final String MOVIE_BASE_URL = "https://api.themoviedb.org/3/movie/";
-    public static final String API_KEY = "6b18b27e909b5ae9e35483e5f57cd2b3";
+    //TODO : add your API KEY HERE
+    public static final String API_KEY = "";
     public static final String TOP_RATED = "top_rated/";
     public static final String POPULAR="popular/";
     public static final String IMAGE_PATH = "http://image.tmdb.org/t/p/w185/";
