927e659b97745f6aa6ebad3892308e3ddf4ad5ef
Mahmoud-AbdelFatah <abdelfatahm3@gmail.com>
2017-04-01 00:53:06 +0200

    stage1

--- app/src/main/java/com/example/android/moviesapp/SettingsFragment.java
+++ app/src/main/java/com/example/android/moviesapp/SettingsFragment.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.android.moviesapp;
 
 import android.os.Bundle;
@@ -8,26 +7,9 @@ import android.support.v7.preference.PreferenceFragmentCompat;
  * Created by Mahmoud on 3/31/2017.
  */
 
-public class SettingsFragment extends PreferenceFragmentCompat{
+public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
         addPreferencesFromResource(R.xml.pref_settings);
     }
 }
-=======
-package com.example.android.moviesapp;
-
-import android.os.Bundle;
-import android.support.v7.preference.PreferenceFragmentCompat;
-
-/**
- * Created by Mahmoud on 3/31/2017.
- */
-
-public class SettingsFragment extends PreferenceFragmentCompat{
-    @Override
-    public void onCreatePreferences(Bundle bundle, String s) {
-        addPreferencesFromResource(R.xml.pref_settings);
-    }
-}
->>>>>>> stage1
